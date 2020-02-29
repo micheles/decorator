55a68b5ef1951614c5c37a6d201b1f3b804dbce6
Michele Simionato <michele.simionato@gmail.com>
2020-02-29 06:27:34 +0100

    Updated setup.py [skip CI]

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ if __name__ == '__main__':
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
                        'Programming Language :: Python :: Implementation :: CPython',
                        'Topic :: Software Development :: Libraries',
                        'Topic :: Utilities'],
