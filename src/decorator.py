9bed6133379b947febd6e724172abf9047b75957
Michele Simionato <michele.simionato@gmail.com>
2021-04-02 18:22:50 +0200

    Tried to upload again with twine

--- src/decorator.py
+++ src/decorator.py
@@ -40,7 +40,7 @@ import itertools
 from contextlib import _GeneratorContextManager
 from inspect import getfullargspec, iscoroutinefunction, isgeneratorfunction
 
-__version__ = '5.0.2'
+__version__ = '5.0.3'
 
 DEF = re.compile(r'\s*def\s*([_\w][_\w\d]*)\s*\(')
 POS = inspect.Parameter.POSITIONAL_OR_KEYWORD
