7849ea64d92561625a05b758c4723c927c081e8b
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-22 08:19:59 +0100

    Replaced setup.py with pyproject

--- src/decorator.py
+++ src/decorator.py
@@ -41,7 +41,7 @@ import functools
 from contextlib import _GeneratorContextManager
 from inspect import getfullargspec, iscoroutinefunction, isgeneratorfunction
 
-__version__ = '5.1.1'
+__version__ = '5.2.0'
 
 DEF = re.compile(r'\s*def\s*([_\w][_\w\d]*)\s*\(')
 POS = inspect.Parameter.POSITIONAL_OR_KEYWORD
